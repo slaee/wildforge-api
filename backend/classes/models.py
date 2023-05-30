b340ed7bb091c126959687f05a4686e127659944
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-05-30 23:55:46 +0800

    Delete classes, models.py to split by files

--- backend/classes/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Class(models.Model):
-    name = models.CharField(max_length=120)
-    sections = models.CharField(max_length=120)
-    schedule = models.CharField(max_length=120)
-    
