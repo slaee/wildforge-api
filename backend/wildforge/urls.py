ef624d3c23aa49cf22d0c749f65abbc1fdc72a04
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-09-22 12:16:18 +0800

    Add admin site

--- backend/wildforge/urls.py
+++ backend/wildforge/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from django.contrib import admin
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view as swagger_get_schema_view
+from rest_framework import permissions
 
 schema_view = swagger_get_schema_view(
     openapi.Info(
@@ -26,9 +28,11 @@ schema_view = swagger_get_schema_view(
         description="API documentation of WildForget API",
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema")
 ]
