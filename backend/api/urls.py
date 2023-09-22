3efd3255aa65b0fd75f03f0f4d9bd831f0bb4203
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-09-22 13:26:07 +0800

    Add tokens/ endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,22 @@
-from django.urls import path, re_path, include
+from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 from .controllers import *
 
 router = routers.DefaultRouter()
 router.register(r'users', UsersController)
 router.register(r'classes', ClassesController)
 urlpatterns = router.urls
+
+urlpatterns += [
+    path('tokens/', include([
+        path('acquire/', TokensController.as_view(), name='acquire_token_pair'),
+        path('refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+        path('verify/', TokenVerifyView.as_view(), name='verify_token'),
+    ])),
+]
