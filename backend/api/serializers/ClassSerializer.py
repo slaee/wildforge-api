84344b4bd4e8a57891d81bf8739f30a8b476af81
acalys <thealphamaximus@gmail.com>
2023-09-24 10:33:28 +0800

    Update ClassesSerializer
    Added autogenerated class_code

--- backend/api/serializers/ClassSerializer.py
+++ backend/api/serializers/ClassSerializer.py
@@ -1,3 +1,5 @@
+import random
+import string
 from rest_framework import serializers
 
 from api.models import Class
@@ -5,9 +7,22 @@ from api.models import Class
 class ClassesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Class
-        fields = ['id', 'name', 'sections', 'schedule']
+        fields = ['id', 'teacherId', 'name', 'sections', 'schedule', 'class_code']
         labels = {
             'name': 'Class Name',
+            'teacherId': 'Teacher',
             'sections': 'Number of Sections',
             'schedule': 'Schedule',
+            'class_code': 'Class Code'
         }
+
+    def create(self, validated_data):
+        # Generate a random alphanumeric class_code
+        class_code_length = 8
+        characters = string.ascii_letters + string.digits  # Alphanumeric characters
+        class_code = ''.join(random.choice(characters) for _ in range(class_code_length))
+        validated_data['class_code'] = class_code
+
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
