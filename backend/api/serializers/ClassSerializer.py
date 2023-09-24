a7c3b9779e0406f3451383dae6d4ef41077fd2e8
acalys <thealphamaximus@gmail.com>
2023-09-24 13:21:47 +0800

    Update create function for class_code validation

--- backend/api/serializers/ClassSerializer.py
+++ backend/api/serializers/ClassSerializer.py
@@ -17,12 +17,14 @@ class ClassesSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data):
-        # Generate a random alphanumeric class_code
-        class_code_length = 8
-        characters = string.ascii_letters + string.digits  # Alphanumeric characters
-        class_code = ''.join(random.choice(characters) for _ in range(class_code_length))
-        validated_data['class_code'] = class_code
+        while True: # Keep generating class_code until a unique one is generated
+            class_code_length = 8
+            characters = string.ascii_letters + string.digits  # Alphanumeric characters
+            class_code = ''.join(random.choice(characters) for _ in range(class_code_length))
 
-        instance = self.Meta.model(**validated_data)
-        instance.save()
-        return instance
+            # Check if the generated class_code already exists in the database
+            if not Class.objects.filter(class_code=class_code).exists():
+                validated_data['class_code'] = class_code
+                instance = self.Meta.model(**validated_data)
+                instance.save()
+                return instance
