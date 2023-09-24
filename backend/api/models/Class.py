36e775b93515be09f116db6d1e1d401772bbf549
acalys <thealphamaximus@gmail.com>
2023-09-24 13:48:37 +0800

    Add unique field option for class_code

--- backend/api/models/Class.py
+++ backend/api/models/Class.py
@@ -8,6 +8,6 @@ class Class(models.Model):
     schedule = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
-    class_code = models.CharField(max_length=100) # class code is generated when class is created
+    class_code = models.CharField(max_length=100, unique=True) # class code is generated when class is created
 
     REQUIRED_FIELDS = ['name', 'sections', 'schedule']
