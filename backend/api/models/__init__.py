0aa6d1ba86c27acae09c3d2fbdce330aab92f233
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-09-26 20:25:21 +0800

    Register all new models

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,7 +1,19 @@
 from .User import User
 from .Class import Class
+from .ClassMember import ClassMember
+from .Team import Team
+from .TeamMember import TeamMember
+from .Remarks import Remarks
+from .PeerEval import PeerEval
+from .ClassAssigned import ClassAssigned
 
 __all__ = [
     'User',
     'Class',
+    'ClassMember',
+    'Team',
+    'TeamMember',
+    'Remarks',
+    'PeerEval',
+    'ClassAssigned',
 ]
