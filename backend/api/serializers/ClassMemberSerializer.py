0565a6431d9f2ab9edf9413d5d9311b5e01fe939
acalys <thealphamaximus@gmail.com>
2023-09-28 16:42:44 +0800

    Add create function for ClassMemberSerializer

--- backend/api/serializers/ClassMemberSerializer.py
+++ backend/api/serializers/ClassMemberSerializer.py
@@ -1,8 +1,29 @@
 from rest_framework import serializers
 
-from api.models import ClassMember
+from api.models import ClassMember, User, Class
 
 class ClassMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = ClassMember
-        pass
+        fields = ['id', 'user_id','class_id', 'is_teacher']
+        labels = {
+            'user_id': 'User ID',
+            'class_id': 'Class ID',
+            'is_teacher': 'User is a teacher?'
+        }
+
+    def create(self, validated_data):
+        user_id = validated_data.get('user_id')
+        class_id = validated_data.get('class_id')
+
+        # Check if user_id and class_id exist in the database
+        if not User.objects.filter(id=user_id).exists():
+            raise serializers.ValidationError("User ID does not exist.")
+
+        if not Class.objects.filter(id=class_id).exists():
+            raise serializers.ValidationError("Class ID does not exist.")
+
+        # Create the instance if both IDs exist
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
