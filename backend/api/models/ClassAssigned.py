0da70b083d5b98c013678d2c4d92c3349a8dca05
acalys <thealphamaximus@gmail.com>
2023-09-28 16:53:45 +0800

    Update foreign keys to default null value

--- backend/api/models/ClassAssigned.py
+++ backend/api/models/ClassAssigned.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class ClassAssigned(models.Model):
-    peer_eval_id = models.ForeignKey('PeerEval', on_delete=models.CASCADE)
-    class_id = models.ForeignKey('Class', on_delete=models.CASCADE)
+    peer_eval_id = models.ForeignKey('PeerEval', on_delete=models.CASCADE, null=True)
+    class_id = models.ForeignKey('Class', on_delete=models.CASCADE, null=True)
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
 
