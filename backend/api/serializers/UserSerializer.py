4d52c5162063ba60154cfaa44798e8804164e81e
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-09-29 02:33:56 +0800

    Add SuperUserSerializer

--- backend/api/serializers/UserSerializer.py
+++ backend/api/serializers/UserSerializer.py
@@ -11,7 +11,8 @@ class UserSerializer(serializers.ModelSerializer):
             'last_name': 'Last Name',
             'email': 'Email',
             'password': 'Password',
-            'is_staff': 'Is Staff'
+            'is_staff': 'Is Staff',
+            'is_superuser': 'Is Superuser'
         }
 
         # set default values
@@ -27,3 +28,16 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+    
+class SuperUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email', 'is_staff', 'is_superuser']
+        # make all read-only fields
+        extra_kwargs = {
+            'first_name': {'read_only': True, 'required': False},
+            'last_name': {'read_only': True, 'required': False},
+            'email': {'read_only': True, 'required': False},
+            'is_staff': {'read_only': True, 'required': False},
+            'is_superuser': {'read_only': True, 'required': False}
+        }
