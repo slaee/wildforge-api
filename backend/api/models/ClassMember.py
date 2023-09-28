42b4c47f45b91701e69afc2ec7e69e529f02ea36
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-09-29 02:35:33 +0800

    Update ClassMember Model

--- backend/api/models/ClassMember.py
+++ backend/api/models/ClassMember.py
@@ -4,5 +4,6 @@ class ClassMember(models.Model):
     user_id = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
     class_id = models.ForeignKey('Class', on_delete=models.CASCADE, null=True)
     is_teacher = models.BooleanField(default=False)
+    status = models.CharField(max_length=50, default='pending')
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
