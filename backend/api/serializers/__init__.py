7c56b2f839492f50a7dc5b97d01497231e82c068
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-09-29 02:34:51 +0800

    Register new added serializers

--- backend/api/serializers/__init__.py
+++ backend/api/serializers/__init__.py
@@ -1,5 +1,6 @@
 from .ClassSerializer import ClassSerializer
 from .UserSerializer import UserSerializer
+from .UserSerializer import SuperUserSerializer
 from .AuthTokenObtainPairSerializer import AuthTokenObtainPairSerializer
 from .ClassAssignedSerializer import ClassAssignedSerializer
 from .ClassMemberSerializer import ClassMemberSerializer
@@ -7,3 +8,4 @@ from .TeamSerializer import TeamSerializer
 from .TeamMemberSerializer import TeamMemberSerializer
 from .RemarksSerializer import RemarksSerializer
 from .PeerEvalSerializer import PeerEvalSerializer
+from .ClassSerializer import JoinClassSerializer
