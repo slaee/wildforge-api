bd844ad5ea7a7febe8bfa2a90a775d5db830399d
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-03 09:26:02 +0800

    Add UserController endpoint code docs

--- backend/api/controllers/UsersController.py
+++ backend/api/controllers/UsersController.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets, mixins, permissions
+from rest_framework import viewsets, mixins, permissions, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from api.models import User
 from api.serializers import UserSerializer
@@ -15,28 +16,82 @@ class UsersController(viewsets.GenericViewSet,
     authentication_classes = [JWTAuthentication]
 
     def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        If the action is 'create', allow any user to create a new user.
+        If the action is 'retrieve', 'update', 'partial_update', or 'destroy', only allow authenticated users to access.
+        otherwise, return 403 Forbidden.
+        """
+
         if self.action == 'create':
             return [permissions.AllowAny()]
         elif self.action in ['retrieve', 'update', 'partial_update', 'destroy']:
             return [permissions.IsAuthenticated()]
         return super().get_permissions()
 
-    @swagger_auto_schema(operation_description="POST /users")
+
+    @swagger_auto_schema(
+        operation_summary="Creates a new user",
+        operation_description="POST /users",
+        request_body=UserSerializer,
+        responses={
+            status.HTTP_200_OK: openapi.Response('Created', UserSerializer),
+            status.HTTP_400_BAD_REQUEST: openapi.Response('Bad Request'),
+            status.HTTP_401_UNAUTHORIZED: openapi.Response('Unauthorized'),
+        }
+    )
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
     
-    @swagger_auto_schema(operation_description="GET /users/{id}")
+
+    @swagger_auto_schema(
+        operation_summary="Retrieves a user",
+        operation_description="GET /users/{id}",
+        responses={
+            status.HTTP_200_OK: openapi.Response('OK', UserSerializer),
+            status.HTTP_404_NOT_FOUND: openapi.Response('Not Found'),
+            status.HTTP_401_UNAUTHORIZED: openapi.Response('Unauthorized'),
+        }
+    )
     def retrieve(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
 
-    @swagger_auto_schema(operation_description="PUT /users/{id}")
+
+    @swagger_auto_schema(
+        operation_summary="Updates a user",
+        operation_description="PUT /users/{id}",
+        request_body=UserSerializer,
+        responses={
+            status.HTTP_200_OK: openapi.Response('OK', UserSerializer),
+            status.HTTP_400_BAD_REQUEST: openapi.Response('Bad Request'),
+            status.HTTP_401_UNAUTHORIZED: openapi.Response('Unauthorized'),
+        }
+    )
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
     
-    @swagger_auto_schema(operation_description="PATCH /users/{id}")
+
+    @swagger_auto_schema(
+        operation_summary="Updates a user partially",
+        operation_description="PATCH /users/{id}",
+        request_body=UserSerializer,
+        responses={
+            status.HTTP_200_OK: openapi.Response('OK', UserSerializer),
+            status.HTTP_400_BAD_REQUEST: openapi.Response('Bad Request'),
+            status.HTTP_401_UNAUTHORIZED: openapi.Response('Unauthorized'),
+        }
+    )
     def partial_update(self, request, *args, **kwargs):
         return super().partial_update(request, *args, **kwargs)
     
-    @swagger_auto_schema(operation_description="DELETE /users/{id}")
+
+    @swagger_auto_schema(
+        operation_summary="Deletes a user",
+        operation_description="DELETE /users/{id}",
+        responses={
+            status.HTTP_204_NO_CONTENT: openapi.Response('No Content'),
+            status.HTTP_401_UNAUTHORIZED: openapi.Response('Unauthorized'),
+        }
+    )
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
