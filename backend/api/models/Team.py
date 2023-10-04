459de4dcb956c69bf68d6ae3c1d4c3fbbdf88da0
acalys <thealphamaximus@gmail.com>
2023-10-04 22:33:27 +0800

    Add properties to Team model

--- backend/api/models/Team.py
+++ backend/api/models/Team.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class Team(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    max_members = models.IntegerField(default=4)
+    status = models.IntegerField(default=0)
+
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ['name', 'max_members']
