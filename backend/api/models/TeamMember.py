ff1ea55d791988ab882d0cab82cbc34faa0b017d
acalys <thealphamaximus@gmail.com>
2023-10-04 22:33:39 +0800

    Add properties to TeamMember model

--- backend/api/models/TeamMember.py
+++ backend/api/models/TeamMember.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class TeamMember(models.Model):
-    pass
+    class_member_id = models.ForeignKey('ClassMember', on_delete=models.CASCADE, null=True)
+    team_id = models.ForeignKey('Team', on_delete=models.CASCADE, null=True)
+
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    
