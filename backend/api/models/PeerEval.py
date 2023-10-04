7be544788c4a683fab82433b793367f3d44949d4
acalys <thealphamaximus@gmail.com>
2023-10-04 22:34:29 +0800

    Add properties to PeerEval model

--- backend/api/models/PeerEval.py
+++ backend/api/models/PeerEval.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class PeerEval(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    forms_link = models.TextField(blank=True, null=True)
+
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
