faf2ecf1f214071ade7aba24cc9178041d28b055
acalys <thealphamaximus@gmail.com>
2023-10-04 22:34:38 +0800

    Add properties to Remarks model

--- backend/api/models/Remarks.py
+++ backend/api/models/Remarks.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 class Remarks(models.Model):
-    pass
+    sender_id = models.ForeignKey('ClassMember', on_delete=models.CASCADE, null=True)
+    receiver_id = models.ForeignKey('TeamMember', on_delete=models.CASCADE, null=True)
+    remarks = models.TextField(blank=True, null=True)
+    type = models.IntegerField(blank=True, null=True)
+    is_viewed = models.BooleanField(default=False)
+    is_approved = models.BooleanField(default=False)
+
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
