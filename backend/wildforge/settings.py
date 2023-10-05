ce09e4ad64414f9610a9881ab4554c76b6f77950
r3dh4ir <64179667+slaee@users.noreply.github.com>
2023-10-05 21:07:04 +0800

    Update settings.py

--- backend/wildforge/settings.py
+++ backend/wildforge/settings.py
@@ -154,7 +154,7 @@ load_dotenv(dotenv_path=API_REPO_DIR / 'env' / 'backend.env')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
+DB_HOST = '0.0.0.0'
 DB_PORT = os.getenv('DB_PORT')
 
 DATABASES = {
