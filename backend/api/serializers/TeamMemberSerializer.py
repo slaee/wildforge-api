94f97f39af53fec1cd7a97e6cc6d1170409f6336
acalys <thealphamaximus@gmail.com>
2023-10-05 15:31:37 +0800

    Implement base functions for Team Member

--- backend/api/serializers/TeamMemberSerializer.py
+++ backend/api/serializers/TeamMemberSerializer.py
@@ -5,4 +5,15 @@ from api.models import TeamMember
 class TeamMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = TeamMember
-        pass
+        fields = ['id', 'class_member_id', 'team_id']
+        labels = {
+            'class_member_id': 'Class Member ID',
+            'team_id': 'Team ID'
+        }
+
+    def create(self, validated_data):
+        instance = self.Meta.model(**validated_data)
+        instance.save()
+        return instance
+    
+    
