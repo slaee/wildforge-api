f0536b79eb87101f4ef2dec310c77604cfe0ffc6
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-20 06:37:14 +0800

    Modify Team and TeamMember Models

--- backend/api/models/Team.py
+++ backend/api/models/Team.py
@@ -4,8 +4,7 @@ class Team(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
     max_members = models.IntegerField(default=4)
-    status = models.IntegerField(default=0)
-    recuitment_status = models.IntegerField(default=0)
+    status = models.IntegerField(default=1)
 
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
--- backend/api/models/TeamMember.py
+++ backend/api/models/TeamMember.py
@@ -3,7 +3,6 @@ from django.db import models
 class TeamMember(models.Model):
     class_member_id = models.ForeignKey('ClassMember', on_delete=models.CASCADE, null=True)
     team_id = models.ForeignKey('Team', on_delete=models.CASCADE, null=True)
-    role = models.CharField(max_length=2, default='tl')
     status = models.CharField(max_length=50, default='pending')
 
     date_created = models.DateTimeField(auto_now=True)
