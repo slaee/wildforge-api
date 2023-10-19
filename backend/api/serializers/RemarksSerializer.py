6e3080c683a82507c2dd1e8d54b8c938538dc3a1
acalys <thealphamaximus@gmail.com>
2023-10-19 21:58:52 +0800

    Initialize implementation for Remarks serializer

--- backend/api/serializers/RemarksSerializer.py
+++ backend/api/serializers/RemarksSerializer.py
@@ -5,4 +5,22 @@ from api.models import Remarks
 class RemarksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Remarks
-        pass
+        fields = ['id', 'sender_id', 'receiver_id', 'remarks', 'type', 'is_viewed', 'is_approved']
+        labels = {
+            'sender_id': 'Sender',
+            'receiver_id': 'Receiver',
+            'remarks': 'Remarks',
+            'type': 'Type',
+            'is_viewed': 'Is Viewed',
+            'is_approved': 'Is Approved'
+        }
+
+        # set default values
+        extra_kwargs = {
+            'is_viewed': {'default': False},
+            'is_approved': {'default': False}
+        }
+
+    def create(self, validated_data):
+        remarks = Remarks.objects.create(**validated_data)
+        return remarks
