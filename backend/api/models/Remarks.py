333d61d797549e66cb4902600d2faa9f4787b5dc
acalys <thealphamaximus@gmail.com>
2023-10-19 21:57:59 +0800

    Add properties to Remarks and PeerEval models

--- backend/api/models/Remarks.py
+++ backend/api/models/Remarks.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Remarks(models.Model):
-    sender_id = models.ForeignKey('ClassMember', on_delete=models.CASCADE, null=True)
-    receiver_id = models.ForeignKey('TeamMember', on_delete=models.CASCADE, null=True)
+    sender_id = models.ForeignKey('ClassMember', on_delete=models.CASCADE, related_name='sent_remarks', null=True)
+    receiver_id = models.ForeignKey('ClassMember', on_delete=models.CASCADE, related_name='received_remarks', null=True)
     remarks = models.TextField(blank=True, null=True)
-    type = models.IntegerField(blank=True, null=True)
+    type = models.IntegerField(default=0)
     is_viewed = models.BooleanField(default=False)
     is_approved = models.BooleanField(default=False)
 
