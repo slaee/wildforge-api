c844b99c6a08dd21162725b6e4d79fee5da7c9b0
acalys <thealphamaximus@gmail.com>
2023-10-19 21:58:35 +0800

    Initial implementation for PeerEval serializer

--- backend/api/serializers/PeerEvalSerializer.py
+++ backend/api/serializers/PeerEvalSerializer.py
@@ -1,8 +1,36 @@
 from rest_framework import serializers
+from rest_framework_nested.relations import NestedHyperlinkedRelatedField
 
 from api.models import PeerEval
 
 class PeerEvalSerializer(serializers.ModelSerializer):
     class Meta:
         model = PeerEval
-        pass
+        fields = ['id', 'name', 'forms_link', 'class_id']
+        labels = {
+            'name': 'Peer Evaluation Name',
+            'forms_link': 'Google Forms Link'
+        }
+        
+    class_assigned = NestedHyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='class-class-members-list',
+        parent_lookup_kwargs={'class_pk': 'class_id'}
+    )
+
+    class_id = serializers.PrimaryKeyRelatedField(
+        queryset=PeerEval.objects.all(),
+        source='class_assigned',
+        write_only=True,
+        required=False
+    )
+
+    def create(self, validated_data):
+        class_assigned = validated_data.pop('class_assigned')
+        peer_eval = PeerEval.objects.create(**validated_data)
+        for class_member in class_assigned:
+            peer_eval.class_assigned.add(class_member)
+        return peer_eval
+    
+    
