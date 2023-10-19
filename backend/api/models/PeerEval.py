9d606d0204ac7678e71eb5dbea6b8e130aef62a9
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-20 06:39:03 +0800

    Add PeerEval model

--- backend/api/models/PeerEval.py
+++ backend/api/models/PeerEval.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 class PeerEval(models.Model):
-    pass
+    name = models.CharField(max_length=255)
+    forms_link = models.TextField(blank=False, null=False)
+
+    date_created = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ['name', 'forms_link']
