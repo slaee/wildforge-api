c31923d9bfc4ebfdf1d6d250445224c8e20b0c09
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-22 12:53:35 +0800

    Fix default property default value of Team status

--- backend/api/models/Team.py
+++ backend/api/models/Team.py
@@ -4,7 +4,7 @@ class Team(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
     max_members = models.IntegerField(default=1)
-    status = models.CharField(max_length=10, default='pending')
+    status = models.CharField(max_length=10, default='open')
 
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
