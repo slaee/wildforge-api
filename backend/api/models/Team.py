7ce135ba2a258450fa25b7ef021be592b679238c
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-22 12:51:55 +0800

    Update Team model properties and defautls

--- backend/api/models/Team.py
+++ backend/api/models/Team.py
@@ -3,8 +3,8 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
-    max_members = models.IntegerField(default=4)
-    status = models.IntegerField(default=1)
+    max_members = models.IntegerField(default=1)
+    status = models.CharField(max_length=10, default='pending')
 
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
