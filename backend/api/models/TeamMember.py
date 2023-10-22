d5365128d5cbfc0cad07abf0875192c71c6f6938
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-10-22 12:54:22 +0800

    Modify TeamMember model property

--- backend/api/models/TeamMember.py
+++ backend/api/models/TeamMember.py
@@ -4,7 +4,7 @@ class TeamMember(models.Model):
     class_member_id = models.ForeignKey('ClassMember', on_delete=models.CASCADE, null=True)
     team_id = models.ForeignKey('Team', on_delete=models.CASCADE, null=True)
     role = models.CharField(max_length=1, default='m')
-    status = models.CharField(max_length=50, default='pending')
+    status = models.CharField(max_length=10, default='pending')
 
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
