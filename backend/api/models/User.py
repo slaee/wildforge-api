6cb921355aff31da0f350f6830ce430695e1c0e7
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-12-08 10:17:55 +0800

    Delete migrations

--- backend/api/models/User.py
+++ backend/api/models/User.py
@@ -6,12 +6,26 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
     date_created = models.DateTimeField(auto_now=True)
     date_updated = models.DateTimeField(auto_now=True)
+    
+    ADMIN = 1
+    MODERATOR = 2
+    BASIC = 3
+
+    role_choices = (
+        (ADMIN, 'Admin'),
+        (MODERATOR, 'Moderator'),
+        (BASIC, 'Basic'),
+    )
+
+    role = models.PositiveSmallIntegerField(choices=role_choices, null=True, blank=True)
 
     username = None
     date_joined = None
+    is_active = None
+    is_staff = None
+    is_superuser = None
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'password']
